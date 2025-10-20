188d1a5fcda3741f859b50775dd685cf51b10fc0
UPBC Team <dev@upbc.mx>
2025-10-20 06:22:31 -0700

    Updates

---
+++ assets/manufactura-ZCgPo4OP.js
@@ -0,0 +1,1 @@
+import"./main-Ceu9OxI4.js";import{b as r}from"./initCareerPage-XykrBxI0.js";import"./vendor-Bcn1fxYS.js";r("manufactura");
---
+++ assets/negocios-KJLQcyYV.js
@@ -0,0 +1,1 @@
+import"./main-Ceu9OxI4.js";import{b as o}from"./initCareerPage-XykrBxI0.js";import"./vendor-Bcn1fxYS.js";o("negocios");
