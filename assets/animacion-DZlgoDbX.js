74337f7d5c77628b7c5ec4fe43931da197a83497
UPBC Team <dev@upbc.mx>
2025-10-29 02:58:30 -0700

    Updates

---
+++ assets/animacion-DZlgoDbX.js
@@ -0,0 +1,1 @@
+import"./main-D0TIgEb0.js";import{b as o}from"./initCareerPage-DK1-E01w.js";import"./vendor-Bcn1fxYS.js";o("animacion");
--- assets/educacion-B9GmHA7A.js
+++
@@ -1,1 +0,0 @@
-import"./main-CKAy931l.js";import{b as o}from"./initCareerPage-Or4kV9z7.js";import"./vendor-Bcn1fxYS.js";o("educacion");
